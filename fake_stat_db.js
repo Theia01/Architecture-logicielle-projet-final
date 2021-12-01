9e690c7c35b21e9b0f3d64cbe03d43325d0d39ce
Axelle <guinaudeau.ion1516@gmail.com>
2021-12-02 00:41:29 +0100

    add variable in fake db

--- fake_stat_db.js
+++ fake_stat_db.js
@@ -11,21 +11,21 @@ module.exports = {
   ],
   games: [
     {
-      name: "la game 1",
+      name: "Art",
       gameId: "skufhzuhfz654ef64sef6s68v4s6e4",
       finalScore: 3,
       numberOfQuestions: 5,
       creationDate: "1638397887148",
     },
     {
-      name: "la game 2",
+      name: "Geography",
       gameId: "skufhzuhfz654ef64sef6s68v4s6g4",
       finalScore: 4,
       numberOfQuestions: 5,
       creationDate: "1638397887148",
     },
     {
-      name: "la game 3",
+      name: "Art",
       gameId: "skufhzuhfz654ef64sef6s64864s6e4",
       finalScore: 4,
       numberOfQuestions: 7,
